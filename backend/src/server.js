0c9fd4d428ce2da4b6826c0f26807d53f7b574a9
Issa Abishev <isaabishev2006@gmail.com>
2025-09-01 22:30:25 -0400

    Noter 1.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // middleware
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: "http://localhost:5173",
-    })
+    }),
   );
 }
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
